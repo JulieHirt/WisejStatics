c38284fe0afafe13259108290992fa266b033568
JulieBodette <jr3python@gmail.com>
2026-01-14 12:05:49 -0500

    blank wisej project

---
+++ Page1.cs
@@ -0,0 +1,12 @@
+﻿using Wisej.Web;
+
+namespace WisejStatics
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
